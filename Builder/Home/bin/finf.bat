1057af60eae0a88a6cb658d46c91335682a826b1
tesommer <tesommer@calclipse.com>
2016-12-05 23:30:20 +0100

    finf.bat:
    -- Fixed CP concatenation.

--- Builder/Home/bin/finf.bat
+++ Builder/Home/bin/finf.bat
@@ -1,7 +1,13 @@
 @echo off
+
 setlocal
 
 set "CP="
-for %%I in ("%~dp0..\lib\*.jar") do set "CP=%CP%;%%I"
+for %%I in ("%~dp0..\lib\*.jar") do call :concat %%I
 
 java -cp "%CP%" freecrumbs.finf.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+goto :eof
+
+:concat
+set CP=%CP%;%1
