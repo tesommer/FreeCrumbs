0df89a576d8d77ca65796fbe539b21e1f4f092ec
tesommer <tesommer@calclipse.com>
2016-12-16 15:01:01 +0100

    Home/bin/dups.pl:
    -- Added Home/bin/dups.pl.
    
    Misc.:
    
    -- Minor changes to Finf/help.txt.

---
+++ Builder/Home/bin/dups.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Basename;
+
+my $script = basename($0);
+
+my $config
+= "# This Perl script is used with Finf to print groups of file duplicates.\n"
+. "# Example: $script | finf -c - MyDirectory | $script x\n"
+. "# Here comes some Finf config:\n"
+. "info.format=\${path}\${filename};\${hash}\n";
+
+my %hashes = ();
+
+if (scalar @ARGV > 0) {
+    foreach (<STDIN>) {
+        if (/([^;]*);([^;]*)$/) {
+            chomp(my $file = $1);
+            chomp(my $hash = $2);
+            if (exists $hashes{$hash}) {
+                my $filesref = $hashes{$hash};
+                push(@$filesref, $file);
+            } else {
+                my @files = ($file);
+                $hashes{$hash} = \@files;
+            }
+        }
+    }
+    foreach my $hash (keys %hashes) {
+        my $filesref = $hashes{$hash};
+        if (scalar @$filesref > 1) {
+            foreach my $file (@$filesref) {
+                print "$file\n";
+            }
+            print "\n";
+        }
+    }
+} else {
+    print $config;
+}
