d012b3f7cec1c6ef1a8e0b1a893b6f2b723902da
tesommer <tesommer@calclipse.com>
2017-05-08 22:33:23 +0200

    Macro:
    -- Added two tests to MacrosTest.

--- Macro/src/test/java/freecrumbs/macro/MacrosTest.java
+++ Macro/src/test/java/freecrumbs/macro/MacrosTest.java
@@ -37,6 +37,90 @@ public class MacrosTest {
         assertSplit(line, 4, "ef", "fg", "gh", "\"abcde fgh i jk $\"");
     }
     
+    @Test
+    public void testEvaluateArithmetic() throws MacroException {
+        final Script script = new Script("");
+        script.setVariable("x", 2);
+        script.setVariable("y", 3);
+        Assert.assertEquals(
+                "x + y",
+                5,
+                Macros.evaluateArithmetic(script, "x", "+", "y"));
+        Assert.assertEquals(
+                "x - 21",
+                -19,
+                Macros.evaluateArithmetic(script, "x", "-", "21"));
+        Assert.assertEquals(
+                "22 * y",
+                66,
+                Macros.evaluateArithmetic(script, "22", "*", "y"));
+        Assert.assertEquals(
+                "23 / 7",
+                3,
+                Macros.evaluateArithmetic(script, "23", "/", "7"));
+        Assert.assertEquals(
+                "23 % 7",
+                2,
+                Macros.evaluateArithmetic(script, "23", "%", "7"));
+        try {
+            Macros.evaluateArithmetic(script, "27", "/", "0");
+            Assert.fail("27 / 0: Expected MacroException.");
+        } catch (final MacroException ex) {
+            Assert.assertTrue(
+                    "Cause: 27 / 0",
+                    ex.getCause() instanceof ArithmeticException);
+        }
+        try {
+            Macros.evaluateArithmetic(script, "26", "%", "0");
+            Assert.fail("26 % 0: Expected MacroException.");
+        } catch (final MacroException ex) {
+            Assert.assertTrue(
+                    "Cause: 26 % 0",
+                    ex.getCause() instanceof ArithmeticException);
+        }
+        try {
+            Macros.evaluateArithmetic(script, "11", "£", "73");
+            Assert.fail("11 £ 73: Expected MacroException.");
+        } catch (final MacroException ex) {
+            Assert.assertNull(
+                    "Cause: 11 £ 73",
+                    ex.getCause());
+        }
+    }
+    
+    @Test
+    public void testEvaluateLogical() throws MacroException {
+        final Script script = new Script("");
+        script.setVariable("x", 7);
+        script.setVariable("y", 11);
+        Assert.assertTrue(
+                "x == 7",
+                Macros.evaluateLogical(script, "x", "==", "7"));
+        Assert.assertTrue(
+                "x != y",
+                Macros.evaluateLogical(script, "x", "!=", "y"));
+        Assert.assertTrue(
+                "7 <= x",
+                Macros.evaluateLogical(script, "7", "<=", "x"));
+        Assert.assertFalse(
+                "11 < y",
+                Macros.evaluateLogical(script, "11", "<", "y"));
+        Assert.assertFalse(
+                "21 >= 23",
+                Macros.evaluateLogical(script, "21", ">=", "23"));
+        Assert.assertTrue(
+                "3 > 2",
+                Macros.evaluateLogical(script, "3", ">", "2"));
+        try {
+            Macros.evaluateLogical(script, "37", "¤", "73");
+            Assert.fail("37 ¤ 73: Expected MacroException.");
+        } catch (final MacroException ex) {
+            Assert.assertNull(
+                    "Cause: 37 ¤ 73",
+                    ex.getCause());
+        }
+    }
+    
     private static void assertSplit(
             final String line, final int limit, final String... expecteds) {
         
