cbc5adcceb79a6b31618fc0da84aee887080b77c
tesommer <tesommer@calclipse.com>
2017-08-19 18:51:26 +0200

    Macro: added from/to-x/y parameters to the wait command.

--- Macro/src/main/java/freecrumbs/macro/gesture/Wait.java
+++ Macro/src/main/java/freecrumbs/macro/gesture/Wait.java
@@ -14,9 +14,11 @@ import freecrumbs.macro.Util;
  * Wait for an image within the current screen capture
  * to either appear or disappear.
  * Syntax:
- * {@code wait image [gone=0 [millis=100]]}
+ * {@code wait from-x from-y to-x to-y image [gone=0 [millis=100]]}
  * {@code gone} is non-zero to wait until the image is no longer there.
  * {@code millis} is the delay in milliseconds between checks.
+ * The from/to parameters work the same way as in
+ * {@link freecrumbs.macro.gesture.Scan}.
  * 
  * @author Tone Sommerland
  */
@@ -28,7 +30,7 @@ public class Wait extends Command {
     private static final String DEFAULT_MILLIS = "100";
     
     public Wait() {
-        super(NAME, 1, 3);
+        super(NAME, 5, 7);
     }
 
     @Override
@@ -43,40 +45,50 @@ public class Wait extends Command {
             final Robot robot,
             final String[] params) throws MacroException {
         
-        final BufferedImage image = script.getImages().getOrLoad(params[0]);
+        final BufferedImage image = getImage(script, params);
         final boolean gone = getGone(script, params);
         final int millis = getMillis(script, params);
-        waitLoop(robot, image, gone, millis);
-    }
-    
-    private static boolean getGone(final Script script, final String[] params)
-            throws MacroException {
-        
-        return script.getVariables().valueOf(
-                paramOrDefault(params, 1, DEFAULT_GONE)) != 0;
-    }
-    
-    private static int getMillis(final Script script, final String[] params)
-            throws MacroException {
-        
-        return script.getVariables().valueOf(
-                paramOrDefault(params, 2, DEFAULT_MILLIS));
-    }
-
-    private static void waitLoop(
-            final Robot robot,
-            final BufferedImage image,
-            final boolean gone,
-            final int millis) {
-        
         while (true) {
-            final int[] xy = new Scanner(Util.createScreenCapture(robot))
+            final int[] xy = getScanner(script, robot, params)
                 .xyOf(image, 1);
             if (gone == (xy.length == 0)) {
                 break;
             }
             robot.delay(millis);
         }
+    }
+
+    private static BufferedImage getImage(
+            final Script script, final String[] params) throws MacroException {
+        
+        return script.getImages().getOrLoad(params[4]);
+    }
+    
+    private static boolean getGone(final Script script, final String[] params)
+            throws MacroException {
+        
+        return script.getVariables().valueOf(
+                paramOrDefault(params, 5, DEFAULT_GONE)) != 0;
+    }
+    
+    private static int getMillis(final Script script, final String[] params)
+            throws MacroException {
+        
+        return script.getVariables().valueOf(
+                paramOrDefault(params, 6, DEFAULT_MILLIS));
+    }
+    
+    private static Scanner getScanner(
+            final Script script,
+            final Robot robot,
+            final String[] params) throws MacroException {
+        
+        final int fromX = script.getVariables().valueOf(params[0]);
+        final int fromY = script.getVariables().valueOf(params[1]);
+        final int toX = script.getVariables().valueOf(params[2]);
+        final int toY = script.getVariables().valueOf(params[3]);
+        return new Scanner(
+                Util.createScreenCapture(robot), fromX, fromY, toX, toY);
     }
 
 }
