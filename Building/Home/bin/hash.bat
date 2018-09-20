facf964dab8f528f504a8fe4bbac89d76421e038
tesommer <tesommer@calclipse.com>
2018-09-21 00:08:50 +0200

    Building: build.xml, launcher scripts.

--- Building/Home/bin/hash.bat
+++ Building/Home/bin/hash.bat
@@ -1,13 +1,3 @@
 @echo off
-
 setlocal
-
-set "CP="
-for %%I in ("%~dp0..\lib\*.jar") do call :concat %%I
-
-java -cp "%CP%" freecrumbs.hash.Hash %*
-
-goto :eof
-
-:concat
-set CP=%CP%;%1
+java -p "%~dp0..\lib" -m freecrumbs.hash/freecrumbs.hash.Hash %*
--- Building/Home/bin/macrec.bat
+++ Building/Home/bin/macrec.bat
@@ -1,13 +1,3 @@
 @echo off
-
 setlocal
-
-set "CP="
-for %%I in ("%~dp0..\lib\*.jar") do call :concat %%I
-
-java -cp "%CP%" freecrumbs.macrec.MacroRecorder %*
-
-goto :eof
-
-:concat
-set CP=%CP%;%1
+java -p "%~dp0..\lib" -m freecrumbs.macrec/freecrumbs.macrec.MacroRecorder %*
