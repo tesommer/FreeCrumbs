ee455816325db6c3cb6fae9e7b35934ae7c8708b
tesommer <tesommer@calclipse.com>
2018-10-22 11:55:46 +0200

    Finf: Adding field.Classification + ClassificationTest.

---
+++ Finf/src/main/java/freecrumbs.finf/freecrumbs/finf/field/Classification.java
@@ -0,0 +1,168 @@
+package freecrumbs.finf.field;
+
+import static freecrumbs.finf.field.Classification.Category.BINARY;
+import static freecrumbs.finf.field.Classification.Category.EMPTY;
+import static freecrumbs.finf.field.Classification.Category.TEXT;
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+import freecrumbs.finf.Field;
+import freecrumbs.finf.FieldComputation;
+
+/**
+ * Classifies a file as either text, binary or empty.
+ * The name of this field is {@code "class"}.
+ * The heuristic used by this computation
+ * checks at most {@code limit} bytes to make the determination,
+ * unless the limit is zero or negative,
+ * in which case it reads the entire file.
+ * The file is deemed binary
+ * if the number of non-text-chars exceeds the {@code threshold},
+ * a normalized percentage of total bytes checked.
+ * If the threshold is negative, the file is always binary.
+ * 
+ * @author Tone Sommerland
+ */
+public final class Classification implements FieldComputation {
+    
+    /**
+     * Represents the heuristic used by this computation.
+     * 
+     * @author Tone Sommerland
+     */
+    public static final class Heuristic {
+        
+        private static final int DEFAULT_LIMIT = 512;
+        
+        private static final double DEFAULT_THRESHOLD = .3;
+        
+        private static final int[]
+        DEFAULT_TEXT_CHARS = IntStream.concat(
+                IntStream.rangeClosed(32, 127),
+                IntStream.of('\r', '\n', '\t', '\b', '\f'))
+            .toArray();
+        
+        private static final IntPredicate
+        DEFAULT_IS_TEXT_CHAR
+            = ch -> IntStream.of(DEFAULT_TEXT_CHARS)
+                .anyMatch(i -> i == ch);
+        
+        private final int limit;
+        private final double threshold;
+        private final IntPredicate isTextChar;
+        
+        private Heuristic(
+                final int limit,
+                final double threshold,
+                final IntPredicate isTextChar) {
+            
+            this.limit = limit;
+            this.threshold = threshold;
+            this.isTextChar = requireNonNull(isTextChar, "isTextChar");
+        }
+        
+        private Heuristic() {
+            this(DEFAULT_LIMIT, DEFAULT_THRESHOLD, DEFAULT_IS_TEXT_CHAR);
+        }
+        
+        public Heuristic withLimit(final int limit) {
+            return new Heuristic(limit, this.threshold, this.isTextChar);
+        }
+        
+        public Heuristic withThreshold(final double threshold) {
+            return new Heuristic(this.limit, threshold, this.isTextChar);
+        }
+        
+        public Heuristic withIsTextChar(final IntPredicate isTextChar) {
+            return new Heuristic(this.limit, this.threshold, isTextChar);
+        }
+    }
+
+    /**
+     * Text, binary or empty.
+     * 
+     * @author Tone Sommerland
+     */
+    public static enum Category {
+        TEXT,
+        BINARY,
+        EMPTY,
+    }
+    
+    /**
+     * The default heuristic uses a limit of 512 bytes,
+     * a threshold of .3 (30 percent)
+     * and chars 32–127 + \r, \n, \t, \b and \f to be text chars.
+     */
+    public static final Heuristic DEFAULT_HEURISTIC = new Heuristic();
+    
+    private static final String NAME = "class";
+    
+    private final Heuristic heuristic;
+    private final Function<? super Category, String> value;
+    private int bytesRead;
+    private int binCount;
+    private Category category;
+    
+    private Classification(
+            final Heuristic heuristic,
+            final Function<? super Category, String> value) {
+        
+        this.heuristic = requireNonNull(heuristic, "heuristic");
+        this.value = requireNonNull(value, "value");
+    }
+    
+    public static Field getField(
+            final Heuristic heuristic,
+            final Function<? super Category, String> value) {
+        
+        return Field.getInstance(NAME, new Classification(heuristic, value));
+    }
+
+    @Override
+    public void reset() throws IOException {
+        bytesRead = 0;
+        binCount = 0;
+        category = null;
+    }
+
+    @Override
+    public boolean update(
+            final byte[] input,
+            final int offset,
+            final int length) throws IOException {
+        
+        if (input.length == 0) {
+            category = EMPTY;
+            return false;
+        }
+        final int offsetPlusLength = offset + length;
+        for (int i = offset; i < offsetPlusLength; i++) {
+            if (++bytesRead > heuristic.limit && heuristic.limit > 0) {
+                return false;
+            }
+            final byte ch = input[i];
+            if (ch == 0) {
+                category = BINARY;
+                return false;
+            } else if (!heuristic.isTextChar.test(ch)) {
+                binCount++;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String get() throws IOException {
+        if (category == null) {
+            category = binCount / (double)bytesRead > heuristic.threshold
+                    ? BINARY : TEXT;
+        }
+        return value.apply(category);
+    }
+
+}
---
+++ Testing/src/test/java/test.freecrumbs/test/freecrumbs/finf/field/ClassificationTest.java
@@ -0,0 +1,178 @@
+package test.freecrumbs.finf.field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import freecrumbs.finf.FieldComputation;
+import freecrumbs.finf.field.Classification;
+
+@DisplayName("Classification")
+public final class ClassificationTest {
+    
+    private static final byte T = 't';
+    private static final byte B = 'b';
+    
+    private static final String
+    EMPTY = Classification.Category.EMPTY.toString();
+    
+    private static final String
+    TEXT = Classification.Category.TEXT.toString();
+    
+    private static final String
+    BINARY = Classification.Category.BINARY.toString();
+
+    public ClassificationTest() {
+    }
+    
+    @Test
+    @DisplayName("Empty")
+    public void test1() throws IOException {
+        final var ups = new byte[][] {
+            new byte[0],
+        };
+        assertClassification(getInstance(), ups, 1, EMPTY);
+    }
+    
+    @Test
+    @DisplayName("Containing null char")
+    public void test2() throws IOException {
+        final var ups = new byte[][] {
+            new byte[] {T, T, T},
+            new byte[] {T, 0, T},
+            new byte[] {T, T, T},
+        };
+        assertClassification(getInstance(), ups, 2, BINARY);
+    }
+    
+    @Test
+    @DisplayName("Exceeding limit")
+    public void test3() throws IOException {
+        final var ups = new byte[][] {
+            new byte[] {T, T, T},
+            new byte[] {T, B, B},
+            new byte[] {B, B, B},
+        };
+        assertClassification(getInstance(4, .3), ups, 2, TEXT);
+    }
+    
+    @Test
+    @DisplayName("Zero limit")
+    public void test4() throws IOException {
+        final var ups = new byte[][] {
+            new byte[] {B, B, B, B, B, B, B, B, B, B, B},
+            new byte[] {B, B, B, B, B, B, B, B, B, B, B},
+            new byte[] {B, B, B, B, B, B, B, B, B, B, B},
+            new byte[] {B, B, B, B, B, B, B, B, B, B, B},
+        };
+        assertClassification(getInstance(0, .3), ups, 4, BINARY);
+    }
+    
+    @Test
+    @DisplayName("Negative limit")
+    public void test5() throws IOException {
+        final var ups = new byte[][] {
+            new byte[] {T, T, T, T, T, T, T, T, T, T, T},
+            new byte[] {T, T, T, T, T, T, T, T, T, T, T},
+            new byte[] {T, T, T, T, T, T, T, T, T, T, T},
+            new byte[] {T, T, T, T, T, T, T, T, T, T, T},
+        };
+        assertClassification(getInstance(-1, .3), ups, 4, TEXT);
+    }
+    
+    @Test
+    @DisplayName("Below threshold")
+    public void test6() throws IOException {
+        final var ups = new byte[][] {
+            new byte[] {T, B, B, T, T, T, B, T, T, T, B},
+            new byte[] {T, B, B, T, T, T, T, T, T, B, B},
+            new byte[] {B, T, T, B, T, T, T, T, T, B, B},
+            new byte[] {B, T, T, T, T, T, T, T, T, T, T},
+        };
+        assertClassification(getInstance(1024, .3), ups, 4, TEXT);
+    }
+    
+    @Test
+    @DisplayName("Exceeding threshold")
+    public void test7() throws IOException {
+        final var ups = new byte[][] {
+            new byte[] {T, B, B, T, T, T, B, T, T, T, B},
+            new byte[] {T, B, B, T, T, T, T, T, T, B, B},
+            new byte[] {B, T, T, B, T, T, T, T, T, B, B},
+            new byte[] {B, T, T, B, T, T, T, T, T, T, T},
+        };
+        assertClassification(getInstance(1024, .3), ups, 4, BINARY);
+    }
+    
+    @Test
+    @DisplayName("Zero threshold")
+    public void test8() throws IOException {
+        assertClassification(
+                getInstance(512, 0),
+                new byte[][] {new byte[] {T, T, T}},
+                1,
+                TEXT);
+        assertClassification(
+                getInstance(512, 0),
+                new byte[][] {new byte[] {T, B, T}},
+                1,
+                BINARY);
+    }
+    
+    @Test
+    @DisplayName("Negative threshold")
+    public void test9() throws IOException {
+        assertClassification(
+                getInstance(512, -1),
+                new byte[][] {new byte[] {T, T, T}},
+                1,
+                BINARY);
+        assertClassification(
+                getInstance(512, -1),
+                new byte[][] {new byte[] {T, B, T}},
+                1,
+                BINARY);
+        assertClassification(
+                getInstance(1, -1),
+                new byte[][] {new byte[] {T, T, T}, new byte[] {T, T, T}},
+                1,
+                BINARY);
+    }
+    
+    private static void assertClassification(
+            final FieldComputation comp,
+            final byte[][] updates,
+            final int expectedEnough,
+            final String expectedValue) throws IOException {
+        
+        comp.reset();
+        int actualEnough = updates.length;
+        for (int i = 0; i < updates.length; i++) {
+            if (!comp.update(updates[i], 0, updates[i].length)) {
+                actualEnough = i + 1;
+                break;
+            }
+        }
+        assertEquals(expectedEnough, actualEnough, "Enough updates");
+        assertEquals(expectedValue, comp.get(), "Value");
+    }
+    
+    private static FieldComputation getInstance(
+            final int limit, final double threshold) {
+        
+        final var heuristic = Classification.DEFAULT_HEURISTIC
+                .withLimit(limit)
+                .withThreshold(threshold)
+                .withIsTextChar(ch -> ch == T);
+        return Classification.getField(heuristic, String::valueOf)
+                .computation();
+    }
+    
+    private static FieldComputation getInstance() {
+        return getInstance(512, .3);
+    }
+
+}
