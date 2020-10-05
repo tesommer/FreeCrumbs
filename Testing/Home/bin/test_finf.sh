bfc289626e94ccd36606f2f95ed4f46ad734b6d1
tone <tone@free.crumbs>
2020-10-05 10:25:31 +0200

    Testing: test_finf.sh

--- Testing/Home/bin/test_finf.sh
+++ Testing/Home/bin/test_finf.sh
@@ -7,94 +7,106 @@ MY_FILENAME="${ME#$MY_DIR/}"
 HOME_DIR="$MY_DIR/.."
 LIB_DIR="$HOME_DIR/lib"
 
-finf() {
+finf()
+{
     $MY_DIR/finf "$@"
 }
 
-fail() {
+fail()
+{
     RED=1
-    echo -e "${FUNCNAME[1]}:\n\t$1"
+    echo -e "${FUNCNAME[-2]}:\n\t$1"
     return 1
 }
 
-zero_args_yields_success_and_no_output() {
-    local OUTPUT
-    OUTPUT=`finf` || fail "exit status"
-    [ -z "$OUTPUT" ] || fail "output: $OUTPUT"
+assert_equals_str()
+{
+    [ "$1" = "$2" ] || fail "expected: $1, actual $2"
 }
 
-exit_status_is_nonzero_on_error() {
+assert_equals_int()
+{
+    [ "$1" -eq "$2" ] || fail "expected: $1, actual $2"
+}
+
+zero_args_yields_success_and_no_output()
+{
+    ACTUAL=`finf` || fail "exit status"
+    [ -z "$ACTUAL" ] || fail "output: $ACTUAL"
+}
+
+exit_status_is_nonzero_on_error()
+{
     finf -o 'filter=*' 2> /dev/null && fail "exit status"
 }
 
-default_config_just_outputs_filenames() {
-    local OUTPUT
-    OUTPUT=`finf $ME` || fail "exit status"
-    [ "$OUTPUT" = "$MY_FILENAME" ] || fail "output: $OUTPUT"
+default_config_just_outputs_filenames()
+{
+    ACTUAL=`finf $ME` || fail "exit status"
+    assert_equals_str "$MY_FILENAME" "$ACTUAL"
 }
 
-config_setting_can_be_overridden_with_option() {
-    local OUTPUT
-    OUTPUT=`finf -o output=Abc $ME` || fail "exit status"
-    [ "$OUTPUT" = "Abc" ] || fail "output: $OUTPUT"
+config_setting_can_be_overridden_with_option()
+{
+    ACTUAL=`finf -o output=Abc $ME` || fail "exit status"
+    assert_equals_str "Abc" "$ACTUAL"
 }
 
-omitting_overridden_value_reverts_setting_to_default() {
-    local OUTPUT
-    OUTPUT=`echo "output=Xyz" | finf -c - -o "output" $ME`
-    [ "$OUTPUT" = "$MY_FILENAME" ] || fail "output: $OUTPUT"
+omitting_overridden_value_reverts_setting_to_default()
+{
+    ACTUAL=`echo "output=Xyz" | finf -c - -o "output" $ME`
+    assert_equals_str "$MY_FILENAME" "$ACTUAL"
 }
 
-order_setting_is_a_list_of_fields_to_order_by() {
-    local OUTPUT
-    local EXPECTED
-    OUTPUT=`finf -o 'order=path filename' "$LIB_DIR"`
+order_setting_is_a_list_of_fields_to_order_by()
+{
     EXPECTED=`ls -1 "$LIB_DIR"`
-    [ "$OUTPUT" = "$EXPECTED" ] || fail "output: $OUTPUT expected: $EXPECTED"
+    ACTUAL=`finf -o 'order=path filename' "$LIB_DIR"`
+    assert_equals_str "$EXPECTED" "$ACTUAL"
 }
 
-fields_in_order_setting_can_have_tailing_asc_or_desc() {
-    local OUTPUT
-    OUTPUT=`finf -o 'order=path asc filename desc' "$LIB_DIR"`
+fields_in_order_setting_can_have_trailing_asc_or_desc()
+{
     EXPECTED=`ls -1 -r "$LIB_DIR"`
-    [ "$OUTPUT" = "$EXPECTED" ] || fail "output: $OUTPUT expected: $EXPECTED"
+    ACTUAL=`finf -o 'order=path asc filename desc' "$LIB_DIR"`
+    assert_equals_str "$EXPECTED" "$ACTUAL"
 }
 
-regex_filter_setting_excludes_input_files_with_names_matching_regex() {
-    local OUTPUT
-    OUTPUT=`finf -o 'filter=.*calclipse.*' "$LIB_DIR"`
-    [ "${OUTPUT:4:9}" = "calclipse" ] || fail "output: $OUTPUT"
+regex_filter_setting_excludes_input_files_with_names_matching_regex()
+{
+    ACTUAL=`finf -o 'filter=.*calclipse.*' "$LIB_DIR"`
+    [ "${ACTUAL:4:9}" = "calclipse" ] || fail "output: $ACTUAL"
 }
 
-multiple_filters_are_supported_by_appending_dot_and_something_to_their_keys() {
-    local OUTPUT
-    OUTPUT=`finf -o 'filter.1=.*jar' -o 'filter.2=.*calclipse.*' "$HOME_DIR"`
-    [ "${OUTPUT:4:9}" = "calclipse" ] || fail "output: $OUTPUT"
+multiple_filters_are_supported_by_appending_dot_and_something_to_their_keys()
+{
+    ACTUAL=`finf -o 'filter.1=.*jar' -o 'filter.2=.*calclipse.*' "$HOME_DIR"`
+    [ "${ACTUAL:4:9}" = "calclipse" ] || fail "output: $ACTUAL"
 }
 
-filter_setting_supports_format_pattern_style() {
-    local OUTPUT
-    OUTPUT=`finf -o 'filter=<filename>++finf.*--.*\.bat' "$HOME_DIR"`
-    [ "$OUTPUT" = "finf" ] || fail "output: $OUTPUT"
+filter_setting_supports_format_pattern_style()
+{
+    ACTUAL=`finf -o 'filter=<filename>++finf.*--.*\.bat' "$HOME_DIR"`
+    [ "$ACTUAL" = "finf" ] || fail "output: $ACTUAL"
 }
 
-date_format_setting_is_applied_to_time_fields() {
-    local OUTPUT
-    OUTPUT=`finf -o 'date.format=yyyy-MM' -o 'output=<modified>' "$ME"`
-    [[ "$OUTPUT" =~ [0-9]+-[0-9]{2} ]] || fail "output: $OUTPUT"
+date_format_setting_is_applied_to_time_fields()
+{
+    ACTUAL=`finf -o 'date.format=yyyy-MM' -o 'output=<modified>' "$ME"`
+    [[ "$ACTUAL" =~ [0-9]+-[0-9]{2} ]] || fail "output: $ACTUAL"
 }
 
-empty_date_format_turns_timestamp_formatting_of() {
-    local OUTPUT
-    OUTPUT=`finf -o 'date.format=' -o 'output=<modified>' "$ME"`
-    [[ "$OUTPUT" =~ [0-9]+ ]] || fail "output: $OUTPUT"
+empty_date_format_turns_timestamp_formatting_of()
+{
+    ACTUAL=`finf -o 'date.format=' -o 'output=<modified>' "$ME"`
+    [[ "$ACTUAL" =~ [0-9]+ ]] || fail "output: $ACTUAL"
 }
 
-eolcount_field_is_the_number_of_line_terminators() {
-    local OUTPUT
-    OUTPUT=`finf -o 'output=<eolcount> <path><filename>' "$ME"`
+eolcount_field_is_the_number_of_line_terminators()
+{
     EXPECTED=`wc -l "$ME"`
-    [ "$OUTPUT" = "$EXPECTED" ] || fail "output: $OUTPUT expected: $EXPECTED"
+    ACTUAL=`finf -o 'output=<eolcount> <path><filename>' "$ME"`
+    assert_equals_str "$EXPECTED" "$ACTUAL"
 }
 
 zero_args_yields_success_and_no_output
@@ -103,7 +115,7 @@ default_config_just_outputs_filenames
 config_setting_can_be_overridden_with_option
 omitting_overridden_value_reverts_setting_to_default
 order_setting_is_a_list_of_fields_to_order_by
-fields_in_order_setting_can_have_tailing_asc_or_desc
+fields_in_order_setting_can_have_trailing_asc_or_desc
 regex_filter_setting_excludes_input_files_with_names_matching_regex
 multiple_filters_are_supported_by_appending_dot_and_something_to_their_keys
 filter_setting_supports_format_pattern_style
@@ -111,4 +123,4 @@ date_format_setting_is_applied_to_time_fields
 empty_date_format_turns_timestamp_formatting_of
 eolcount_field_is_the_number_of_line_terminators
 
-([ $RED -eq 0 ] && echo "GREEN") || echo "RED"
+[ "$RED" -eq "0" ] && echo "GREEN" || echo "RED"
